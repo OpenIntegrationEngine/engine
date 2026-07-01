d0c9c6b21441048434cf98cf5274ce49c4427760
Joachim Beckers <joachim.beckers@nexuzhealth.com>
2026-07-01 20:05:22 +0200

    SPDX'ify license and copyright headers
    Signed-off-by: Joachim Beckers <joachim.beckers@nexuzhealth.com>

--- server/src/com/mirth/connect/server/util/ConnectorUtil.java
+++ server/src/com/mirth/connect/server/util/ConnectorUtil.java
@@ -1,11 +1,5 @@
-/*
- * Copyright (c) Mirth Corporation. All rights reserved.
- *
- * http://www.mirthcorp.com
- *
- * The software in this package is published under the terms of the MPL license a copy of which has
- * been included with this distribution in the LICENSE.txt file.
- */
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: Mirth Corporation
 
 package com.mirth.connect.server.util;
 
